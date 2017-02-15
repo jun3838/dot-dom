5307d3cf0e67dfb35e0223fa18dd46426110e8b2
Ioannis Charalampidis <ioannis.charalampidis@cern.ch>
2017-02-15 11:12:08 +0100

    Using $ as element name & special prop -> Reduced to 504 bytes

--- src/__tests__/dotdom-test.js
+++ src/__tests__/dotdom-test.js
@@ -10,14 +10,14 @@ describe('.dom', function () {
       it('should create vnode without arguments', function () {
         const vdom = dd.H('div');
 
-        expect(vdom.E).toEqual('div');
+        expect(vdom.$).toEqual('div');
         expect(vdom.P).toEqual({C: []});
       });
 
       it('should create vnode with props', function () {
         const vdom = dd.H('div', {foo: 'bar'});
 
-        expect(vdom.E).toEqual('div');
+        expect(vdom.$).toEqual('div');
         expect(vdom.P).toEqual({foo: 'bar', C: []});
       });
 
@@ -25,7 +25,7 @@ describe('.dom', function () {
         const cdom = dd.H('div');
         const vdom = dd.H('div', {foo: 'bar'}, cdom);
 
-        expect(vdom.E).toEqual('div');
+        expect(vdom.$).toEqual('div');
         expect(vdom.P).toEqual({
           foo: 'bar',
           C: [ cdom ]
@@ -38,7 +38,7 @@ describe('.dom', function () {
         const cdom3 = dd.H('div');
         const vdom = dd.H('div', {foo: 'bar'}, cdom1, [cdom2, cdom3]);
 
-        expect(vdom.E).toEqual('div');
+        expect(vdom.$).toEqual('div');
         expect(vdom.P).toEqual({
           foo: 'bar',
           C: [ cdom1, cdom2, cdom3 ]
@@ -49,7 +49,7 @@ describe('.dom', function () {
         const cdom = dd.H('div');
         const vdom = dd.H('div', {foo: 'bar'}, 'foo', cdom);
 
-        expect(vdom.E).toEqual('div');
+        expect(vdom.$).toEqual('div');
         expect(vdom.P).toEqual({
           foo: 'bar',
           C: [ 'foo', cdom ]
@@ -59,7 +59,7 @@ describe('.dom', function () {
       it('should create vnode with props and string children', function () {
         const vdom = dd.H('div', {foo: 'bar'}, 'foo');
 
-        expect(vdom.E).toEqual('div');
+        expect(vdom.$).toEqual('div');
         expect(vdom.P).toEqual({
           foo: 'bar',
           C: [ 'foo' ]
@@ -69,7 +69,7 @@ describe('.dom', function () {
       it('should create vnode with only child', function () {
         const vdom = dd.H('div', 'foo');
 
-        expect(vdom.E).toEqual('div');
+        expect(vdom.$).toEqual('div');
         expect(vdom.P).toEqual({
           C: [ 'foo' ]
         });
@@ -78,7 +78,7 @@ describe('.dom', function () {
       it('should create vnode with children', function () {
         const vdom = dd.H('div', 'foo', 'bar', 'baz');
 
-        expect(vdom.E).toEqual('div');
+        expect(vdom.$).toEqual('div');
         expect(vdom.P).toEqual({
           C: [ 'foo', 'bar', 'baz' ]
         });
@@ -87,7 +87,7 @@ describe('.dom', function () {
       it('should create vnode with children in arrays', function () {
         const vdom = dd.H('div', 'foo', ['bar', 'baz']);
 
-        expect(vdom.E).toEqual('div');
+        expect(vdom.$).toEqual('div');
         expect(vdom.P).toEqual({
           C: [ 'foo', 'bar', 'baz' ]
         });
@@ -97,7 +97,7 @@ describe('.dom', function () {
         const cdom = dd.H('div');
         const vdom = dd.H('div', cdom, 'foo');
 
-        expect(vdom.E).toEqual('div');
+        expect(vdom.$).toEqual('div');
         expect(vdom.P).toEqual({
           C: [ cdom, 'foo' ]
         });
@@ -111,7 +111,7 @@ describe('.dom', function () {
         const cdom = dd.H('div');
         const vdom = dd.H.apply({}, ['div', cdom, 'foo']);
 
-        expect(vdom.E).toEqual('div');
+        expect(vdom.$).toEqual('div');
         expect(vdom.P).toEqual({
           C: [ cdom, 'foo' ]
         });
@@ -121,7 +121,7 @@ describe('.dom', function () {
         const cdom = dd.H('div');
         const vdom = dd.H.call({}, 'div', cdom, 'foo');
 
-        expect(vdom.E).toEqual('div');
+        expect(vdom.$).toEqual('div');
         expect(vdom.P).toEqual({
           C: [ cdom, 'foo' ]
         });
@@ -131,7 +131,7 @@ describe('.dom', function () {
         const cdom = dd.H('div');
         const vdom = dd.H.div(cdom, 'foo');
 
-        expect(vdom.E).toEqual('div');
+        expect(vdom.$).toEqual('div');
         expect(vdom.P).toEqual({
           C: [ cdom, 'foo' ]
         });
